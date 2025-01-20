0cdbf718e82e50890935a8ee9c121057a394d644
Jonathan Vanderwater <jonathan.vanderwater@gmail.com>
2025-01-19 22:36:50 -0600

    Change comment for readability

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -5,23 +5,14 @@ let
   githubUsername = "vanderwaterj";
 in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "vanderwaterj";
   home.homeDirectory = "/home/vanderwaterj";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  # Check the Home Manager release notes before changing stateVersion
+  home.stateVersion = "24.11";
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
